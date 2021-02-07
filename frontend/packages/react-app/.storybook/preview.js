248cf40057a19b2c3c2bbcc8c0f17ca7d9fb0b47
MaOh <maoh.design@gmail.com>
2021-02-07 22:41:15 +0900

    Update Storybook config and Delete unused css

--- frontend/packages/react-app/.storybook/preview.js
+++ frontend/packages/react-app/.storybook/preview.js
@@ -2,3 +2,18 @@ export const parameters = {
   layout: "fullscreen",
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+import React from "react"
+import { StylesProvider } from "@material-ui/styles"
+import { ThemeProvider } from "@material-ui/core/styles"
+import { theme } from "../src/components/util/style/theme"
+
+export const decorators = [
+  (Story) => (
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    </StylesProvider>
+  ),
+]

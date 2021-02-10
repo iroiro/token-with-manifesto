216b9ac5add2b3a4f9422c4adcb2ebc6ca00a439
toshiakitakase <toshi@tart.tokyo>
2021-02-11 01:50:56 +0900

    check Transfer event

--- subgraph/token-with-manifesto-rinkeby/generated/schema.ts
+++ subgraph/token-with-manifesto-rinkeby/generated/schema.ts
@@ -95,4 +95,111 @@ export class Token extends Entity {
   set witnessDids(value: Array<string>) {
     this.set("witnessDids", Value.fromStringArray(value));
   }
+
+  get accountTokens(): Array<string> {
+    let value = this.get("accountTokens");
+    return value.toStringArray();
+  }
+
+  set accountTokens(value: Array<string>) {
+    this.set("accountTokens", Value.fromStringArray(value));
+  }
+}
+
+export class Account extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save Account entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save Account entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("Account", id.toString(), this);
+  }
+
+  static load(id: string): Account | null {
+    return store.get("Account", id) as Account | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get tokens(): Array<string> {
+    let value = this.get("tokens");
+    return value.toStringArray();
+  }
+
+  set tokens(value: Array<string>) {
+    this.set("tokens", Value.fromStringArray(value));
+  }
+}
+
+export class AccountToken extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save AccountToken entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save AccountToken entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("AccountToken", id.toString(), this);
+  }
+
+  static load(id: string): AccountToken | null {
+    return store.get("AccountToken", id) as AccountToken | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get account(): string {
+    let value = this.get("account");
+    return value.toString();
+  }
+
+  set account(value: string) {
+    this.set("account", Value.fromString(value));
+  }
+
+  get balance(): BigInt {
+    let value = this.get("balance");
+    return value.toBigInt();
+  }
+
+  set balance(value: BigInt) {
+    this.set("balance", Value.fromBigInt(value));
+  }
+
+  get token(): string {
+    let value = this.get("token");
+    return value.toString();
+  }
+
+  set token(value: string) {
+    this.set("token", Value.fromString(value));
+  }
 }
--- subgraph/token-with-manifesto-rinkeby/src/mapping.ts
+++ subgraph/token-with-manifesto-rinkeby/src/mapping.ts
@@ -1,40 +1,96 @@
 import { BigInt } from "@graphprotocol/graph-ts";
-import {
-  TokenFactory,
-  CreateToken,
-} from "../generated/TokenFactory/TokenFactory";
+import { CreateToken } from "../generated/TokenFactory/TokenFactory";
 import { TokenWithManifesto } from "../generated/TokenFactory/TokenWithManifesto";
-import { Token } from "../generated/schema";
+import { Token, Account, AccountToken } from "../generated/schema";
+import { TokenWithManifesto as TokenWithManifestoTemplate } from "../generated/templates";
+import { Transfer } from "../generated/templates/TokenWithManifesto/TokenWithManifesto";
 
 export function handleCreateToken(event: CreateToken): void {
-  let entity = Token.load(event.transaction.from.toHex());
-  if (entity == null) {
-    entity = new Token(event.transaction.from.toHex());
-    entity.id = event.params.token.toHex();
-
-    let TWM = TokenWithManifesto.bind(event.params.token);
-
-    let name = TWM.try_name();
-    if (!name.reverted) {
-      entity.name = name.value;
-    }
-
-    let symbol = TWM.try_symbol();
-    if (!symbol.reverted) {
-      entity.symbol = symbol.value;
-    }
-
-    let decimals = TWM.try_decimals();
-    if (!decimals.reverted) {
-      entity.decimals = decimals.value;
-    }
-
-    let totalSupply = TWM.try_totalSupply();
-    if (!totalSupply.reverted) {
-      entity.totalSupply = totalSupply.value;
-    }
-    entity.creator = event.params.creator;
-    entity.witnessDids = event.params.witnessDids;
+  let token = Token.load(event.transaction.from.toHex());
+  if (token == null) {
+    token = new Token(event.transaction.from.toHex());
   }
-  entity.save();
+
+  let TWM = TokenWithManifesto.bind(event.params.token);
+  token.id = event.params.token.toHex();
+
+  let name = TWM.try_name();
+  if (!name.reverted) {
+    token.name = name.value;
+  }
+
+  let symbol = TWM.try_symbol();
+  if (!symbol.reverted) {
+    token.symbol = symbol.value;
+  }
+
+  let decimals = TWM.try_decimals();
+  if (!decimals.reverted) {
+    token.decimals = decimals.value;
+  }
+
+  let totalSupply = TWM.try_totalSupply();
+  if (!totalSupply.reverted) {
+    token.totalSupply = totalSupply.value;
+  }
+
+  token.creator = event.params.creator;
+  token.witnessDids = event.params.witnessDids;
+  token.accountTokens = [];
+  token.save();
+
+  TokenWithManifestoTemplate.create(event.params.token);
+}
+
+export function handleTransfer(event: Transfer): void {
+  let tokenId = event.address.toHex();
+  let token = Token.load(tokenId);
+  let fromAccount = Account.load(event.params.from.toHex());
+  if (fromAccount == null) {
+    fromAccount = new Account(event.params.from.toHex());
+  }
+  fromAccount.save();
+
+  let toAccount = Account.load(event.params.to.toHex());
+  if (toAccount == null) {
+    toAccount = new Account(event.params.to.toHex());
+  }
+  toAccount.save();
+
+  let fromAccountTokenId = event.params.from
+    .toHexString()
+    .concat("-")
+    .concat(tokenId);
+  let fromAccountToken = AccountToken.load(fromAccountTokenId);
+  if (fromAccountToken == null) {
+    fromAccountToken = new AccountToken(fromAccountTokenId);
+    fromAccountToken.balance = BigInt.fromI32(0);
+  }
+  fromAccountToken.account = fromAccount.id;
+  fromAccountToken.balance = fromAccountToken.balance.minus(event.params.value);
+  fromAccountToken.save();
+
+  let toAccountTokenId = event.params.to
+    .toHexString()
+    .concat("-")
+    .concat(tokenId);
+  let toAccountToken = AccountToken.load(toAccountTokenId);
+  if (toAccountToken == null) {
+    toAccountToken = new AccountToken(toAccountTokenId);
+    toAccountToken.balance = BigInt.fromI32(0);
+  }
+  toAccountToken.account = toAccount.id;
+  toAccountToken.balance = toAccountToken.balance.plus(event.params.value);
+  toAccountToken.save();
+
+  let tmpAccountsTokens = token.accountTokens;
+  if (!tmpAccountsTokens.includes(fromAccountTokenId)) {
+    tmpAccountsTokens.push(fromAccountTokenId);
+  }
+  if (!tmpAccountsTokens.includes(toAccountTokenId)) {
+    tmpAccountsTokens.push(toAccountTokenId);
+  }
+  token.accountTokens = tmpAccountsTokens;
+
+  token.save();
 }
